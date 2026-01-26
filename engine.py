167543b66f28dda74d688c47a71a95aa5c6b0e64
Mr.Robot <elimassaqui@gmail.com>
2026-01-27 00:34:39 +0100

    bem melhor assim engine mensagens formalizadas

--- engine.py
+++ engine.py
@@ -50,12 +50,12 @@ def initialize_wandi_engine():
         run_cli_clean(["config", "init", "--overwrite"], "Inicializando arquivos de sistema...")
 
     # 3. Sincronização e Instalação/Upgrade do Core AVR
-    run_cli_clean(["core", "update-index"], "Sincronizando banco de dados de Instalação/Upgrade do Core AVR...")
+    run_cli_clean(["core", "update-index"], "Sincronizando banco de dados de Instalação/Upgrade...")
 
     if not os.path.exists(avr_path):
-        run_cli_direct_msg = "Instalando compiladores para familía AVR..."
+        run_cli_direct_msg = "Instalando compiladores da arquitetura AVR..."
         run_cli_clean(["core", "install", "arduino:avr"], run_cli_direct_msg)
     else:
-        run_cli_clean(["core", "upgrade", "arduino:avr"], "Verificando atualizações da familía AVR...")
+        run_cli_clean(["core", "upgrade", "arduino:avr"], "Verificando atualizações do núcleo e placas da arquitetura AVR...")
 
     print("<br><font color='#6a9955'><b>✅ Motor do sistema Wandi preprado com sucesso!</b></font><br>", flush=True)
